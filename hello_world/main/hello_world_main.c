df9d832da827ff7d5e0501c01640d5fed9471ae7
Cesar Rodriguez <cesaresq20@gmail.com>
2022-07-05 18:36:36 -0600

    Adding macros to define fw version

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -13,6 +13,10 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+
 void app_main(void)
 {
     printf("Hello world!\n");
